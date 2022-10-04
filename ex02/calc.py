a74ff215b6792ffcf2250a6c3ea70d2097b35454
Seki Yuto <c0b210990e@edu.teu.ac.jp>
2022-10-04 16:32:46 +0900

    追加機能の追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,8 +7,13 @@ root.geometry("300x500")
 def button_click(event):
     btn = event.widget
     n= btn["text"]
-    tkm.showinfo(f"{n}",f"{n}ボタンがクリックされました")
+    event.widget["bg"] = "red"
+    #tkm.showinfo(f"{n}",f"{n}ボタンがクリックされました")
     entry.insert(tk.END,n)
+
+def right_click(event):
+    entry.delete(0,tk.END)
+    event.widget["bg"] = "white"
 
 def click_equal(event):
     a = entry.get()
@@ -16,20 +21,30 @@ def click_equal(event):
     entry.delete(0,tk.END)
     entry.insert(tk.END,result)
 
-entry = tk.Entry(root,width=10,font=("",40),justify="right")
+
+
+
+
+
+
+
+entry = tk.Entry(root,width=10,font=("",40),justify="right",bg="gray")
 entry.grid(row=0,column=0,columnspan=3)
+
 
 r,c=1,0
 suji = list(range(9,-1,-1))
 enzanshi = ["+"]
 for i,n in enumerate(suji+enzanshi,1):
-    btn = tk.Button(root,text=f"{n}",font=("Times New Roman",30),width=4,height=2)
+    btn = tk.Button(root,text=f"{n}",font=("",30),width=4,height=2)
     btn.bind("<1>",button_click)
+    btn.bind("<3>",right_click)
     btn.grid(row=r,column=c)
     c += 1
     if i%3 == 0:
         r+= 1
         c =0
+    
 
 btn = tk.Button(root,text="=",font=("",30),width=4,height=2)
 btn.bind("<1>",click_equal)
