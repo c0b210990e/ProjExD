cbfd5199ae070fe4dc77488ab88efefa57053e45
Seki Yuto <c0b210990e@edu.teu.ac.jp>
2022-10-04 15:30:05 +0900

    電卓問７まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,13 +10,19 @@ def button_click(event):
     tkm.showinfo(f"{n}",f"{n}ボタンがクリックされました")
     entry.insert(tk.END,n)
 
+def click_equal(event):
+    a = entry.get()
+    result = eval(a)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,result)
+
 entry = tk.Entry(root,width=10,font=("",40),justify="right")
 entry.grid(row=0,column=0,columnspan=3)
 
 r,c=1,0
-s = list(range(9,-1,-1))
-e = ["+","="]
-for i,n in enumerate(s+e,1):
+suji = list(range(9,-1,-1))
+enzanshi = ["+"]
+for i,n in enumerate(suji+enzanshi,1):
     btn = tk.Button(root,text=f"{n}",font=("Times New Roman",30),width=4,height=2)
     btn.bind("<1>",button_click)
     btn.grid(row=r,column=c)
@@ -25,5 +31,7 @@ for i,n in enumerate(s+e,1):
         r+= 1
         c =0
 
-
+btn = tk.Button(root,text="=",font=("",30),width=4,height=2)
+btn.bind("<1>",click_equal)
+btn.grid(row=r,column=c)
 root.mainloop()
