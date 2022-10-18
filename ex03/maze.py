b5d5d811e55c147685b7173ed9b567f2ba05ba53
Seki Yuto <c0b210990e@edu.teu.ac.jp>
2022-10-18 16:34:21 +0900

    タイマー機能の追加

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,13 +4,15 @@ import maze_maker as mm #練習8
 def key_down(event): #練習5
     global key
     key = event.keysym
-
+    root.after(1000,count_up)
 
 def key_up(event): #練習6
     global key
     key = ""
 
+
 def main_proc(): #練習7
+    global tmr
     global mx, my 
     global cx, cy    
     if key == "Up":
@@ -24,6 +26,7 @@ def main_proc(): #練習7
     if maze_list[my][mx] == 0: #床だったら
         cx, cy = mx*100+50, my*100+50
     else: #壁だったら
+        tmr += 10
         if key == "Up":
             my += 1
         if key == "Down":
@@ -35,12 +38,20 @@ def main_proc(): #練習7
     canv.coords("tori", cx, cy)
     root.after(100,main_proc)
 
+def count_up():
+    global tmr
+    tmr += 1
+    label["text"] = tmr
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん") #練習1
     canv = tk.Canvas(root,width = 1500, height = 900, bg = "black") #練習2
+    
     canv.pack()
+    label = tk.Label(root, font=("",80))
+    label.pack()
 
     #練習9
     maze_list = mm.make_maze(15,9)
@@ -53,6 +64,7 @@ if __name__ == "__main__":
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image = tori, tag="tori")
+    
 
     #練習4
     key = "" #現在押されているキーを表す
@@ -63,4 +75,6 @@ if __name__ == "__main__":
     #練習7
     main_proc()
 
+    tmr = 0
+    root.after(1000, count_up)
     root.mainloop()
