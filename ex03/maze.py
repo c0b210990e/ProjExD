7c4345497527ca6e448ffb17b51410c973c6cedf
Seki Yuto <c0b210990e@edu.teu.ac.jp>
2022-10-18 15:20:02 +0900

    練習11

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,15 +11,17 @@ def key_up(event): #練習6
     key = ""
 
 def main_proc(): #練習7
+    global mx, my 
     global cx, cy    
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    cx, cy = mx*100+50, my*100+50
     canv.coords("tori", cx, cy)
     root.after(100,main_proc)
 
@@ -29,8 +31,17 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん") #練習1
     canv = tk.Canvas(root,width = 1500, height = 900, bg = "black") #練習2
     canv.pack()
+
+    #練習9
+    maze_list = mm.make_maze(15,9)
+    print(maze_list) #1:壁/0:床を表す
+
+    #練習10
+    mm.show_maze(canv, maze_list)
+
     tori = tk.PhotoImage(file="fig/6.png") #練習3
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image = tori, tag="tori")
 
     #練習4
@@ -42,10 +53,4 @@ if __name__ == "__main__":
     #練習7
     main_proc()
 
-    #練習9
-    maze_list = mm.make_maze(15,9)
-    print(maze_list) #1:壁/0:床を表す
-
-    #練習10
-    mm.show_maze(canv, maze_list)
     root.mainloop()
