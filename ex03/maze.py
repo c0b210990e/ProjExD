cc66e76eaf3d739aa47a45f4a9177dd8fb43319b
Seki Yuto <c0b210990e@edu.teu.ac.jp>
2022-10-18 14:19:45 +0900

    こうかとんのインスタンス生成

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,4 +5,7 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん") #練習1
     canv = tk.Canvas(root,width = 1500, height = 900, bg = "black")
     canv.pack()
+    tori = tk.PhotoImage(file="fig/6.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image = tori, tag="tori")
     root.mainloop()
