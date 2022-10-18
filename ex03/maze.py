1301cbd9a4a13344ff552608f601306245f7caa6
Seki Yuto <c0b210990e@edu.teu.ac.jp>
2022-10-18 14:21:45 +0900

    練習4

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,9 +3,11 @@ import tkinter as tk
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん") #練習1
-    canv = tk.Canvas(root,width = 1500, height = 900, bg = "black")
+    canv = tk.Canvas(root,width = 1500, height = 900, bg = "black") #練習2
     canv.pack()
-    tori = tk.PhotoImage(file="fig/6.png")
+    tori = tk.PhotoImage(file="fig/6.png") #練習3
     cx, cy = 300, 400
     canv.create_image(cx, cy, image = tori, tag="tori")
+    #練習4
+    key = "" #現在押されているキーを表す
     root.mainloop()
