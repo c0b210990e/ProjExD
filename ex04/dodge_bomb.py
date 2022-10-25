62b4b07808927f9fdb77fc414fe09c86aac8c441
Seki Yuto <c0b210990e@edu.teu.ac.jp>
2022-10-25 14:27:15 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,15 +3,29 @@ import sys
 
 
 def main():
-    pg.display.set_caption("逃げろ！こうかとん") #タイトルバーに「初めてのPyGame」と表示
+    pg.display.set_caption("逃げろ！こうかとん") #タイトルバーに「逃げろ！こうかとん」と表示
     scrn_sfc = pg.display.set_mode((1600,900)) #800×600の画面Surfaceを生成
 
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
-    scrn_sfc.blit(bg_sfc,bg_rct)
-    pg.display.update()
+
+    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc,0,2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+
     clock = pg.time.Clock()
-    clock.tick(0.2)
+
+    while True:
+        scrn_sfc.blit(bg_sfc,bg_rct)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        scrn_sfc.blit(tori_sfc, tori_rct)
+        pg.display.update()
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
