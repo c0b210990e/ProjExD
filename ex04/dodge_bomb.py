85365ee47366ac177d4c75a1fabca7aa37992afe
Seki Yuto <c0b210990e@edu.teu.ac.jp>
2022-10-25 16:56:36 +0900

    爆弾の色変えた

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -29,9 +29,13 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
-    bomb_sfc = pg.Surface((80,80))
+    bomb_sfc = pg.Surface((100,100))
     bomb_sfc.set_colorkey((0,0,0))
-    pg.draw.circle(bomb_sfc,(255,0,0),(40,40),20) #半径10の赤い円
+    pg.draw.circle(bomb_sfc,(255,0,0),(50,50),50) #半径10の赤い円
+    pg.draw.circle(bomb_sfc,(191,0,0),(50,50),40)
+    pg.draw.circle(bomb_sfc,(127,0,0),(50,50),30)
+    pg.draw.circle(bomb_sfc,(63,0,0),(50,50),20)
+    pg.draw.circle(bomb_sfc,(0,0,0),(50,50),10)
     bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx = randint(0, scrn_rct.width)
     bomb_rct.centery = randint(0, scrn_rct.height)
@@ -73,8 +77,8 @@ def main():
         scrn_sfc.blit(bomb_sfc,bomb_rct)
 
         if tori_rct.colliderect(bomb_rct):
-            scrn_sfc.fill((0,0,0)) 
-            fonto = pg.font.Font(None, 200)
+            scrn_sfc.fill((0,0,0)) #画面の色を黒にする 
+            fonto = pg.font.Font(None, 200) #Game Overを表示
             moji = "Game Over"
             txt = fonto.render(str(moji),True,(255,0,0))
             scrn_sfc.blit(txt, (400,450))
